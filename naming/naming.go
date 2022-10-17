e4d33a4536998056a6d3341e21097f470df66650
jayxu96 <31394286+jayxu96@users.noreply.github.com>
2022-10-17 11:47:25 +0800

    Init baby-naming v1

---
+++ naming/naming.go
@@ -0,0 +1,25 @@
+package naming
+// package main
+import (
+	"fmt"
+	"math/rand"
+    "time"
+)
+
+var babyNames = []string{"Mike", "Jay", "Tim", "Wendy", "Elisabeth", "Lacus"}
+
+func CreateBabyName(lastName string) string {
+       rand.Seed(time.Now().UTC().UnixNano())
+       randomize := rand.Intn(len(babyNames))
+       fullName := babyNames[randomize] + " " + lastName
+       return fullName
+}
+
+// func main() {
+//     var lastName string;
+//     fmt.Println("***** This is a Baby Naming Generator *****")
+//     fmt.Println("Please enter your Last Name: " )
+//
+//     fmt.Scanln(&lastName)
+//     fmt.Println("The baby name is " + CreateBabyName(lastName))
+// }
